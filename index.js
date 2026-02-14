8282cd33c5e641f1db3a055463ecfb379a290b39
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-14 18:57:09 +0100

    icons

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const weatherForm = document.querySelector("form");
 
 let cityWeatherContainer = document.querySelector(".weather-data-container");
 
+//Submit
+
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cityInput = document.getElementById("city");
   const cityName = cityInput.value;
 
+  cityWeatherContainer.classList.add("visible");
+  cityWeatherContainer.innerHTML = `<p>Loading weather for ${cityInput.value}...</p>`;
+
   getData(cityName);
 
   cityInput.value = "";
 });
+
+//Get API Data
 
 async function getData(cityName) {
   const baseUrl = import.meta.env.VITE_API_URL;
@@ -31,7 +38,12 @@ async function getData(cityName) {
     //console.log(data);
 
     cityWeatherContainer.classList.add("visible");
+
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
     cityWeatherContainer.innerHTML = `<h2 style="margin: 0;">${data.name}</h2>
+    <img src="${iconUrl}" class="weather-icon" alt="${data.weather[0].description}" />
   <div style="font-size: 3rem; font-weight: bold;">${Math.round(data.main.temp)}°C</div>
   <div style="text-transform: capitalize;">${data.weather[0].description}</div>
       <p>Wind: ${data.wind.speed}m/s</p>
