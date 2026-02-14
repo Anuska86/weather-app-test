015b00c598b8786b347393a9ab4602526d1efbfb
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-14 18:36:52 +0100

    time out

--- index.js
+++ index.js
@@ -2,10 +2,40 @@ import "./index.css";
 
 const weatherForm = document.querySelector("form");
 
+let cityWeatherContainer = document.querySelector(".weather-data-container");
+
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const inputEl = document.getElementById("city").value;
+  const cityName = document.getElementById("city").value;
 
-  console.log("city value:", inputEl);
+  console.log("city name:", cityName);
+
+  getData(cityName);
 });
+
+async function getData(cityName) {
+  const baseUrl = import.meta.env.VITE_API_URL;
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  const finalUrl = `${baseUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await fetch(finalUrl);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    cityWeatherContainer.classList.add("visible");
+    cityWeatherContainer.innerHTML = `<h3>Weather in ${data.name}</h3>
+      <p>Temperature: ${data.main.temp}°C</p>
+      <p>Condition: ${data.weather[0].description}</p>
+      <p>Wind: ${data.wind.speed}</p>
+      `;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
